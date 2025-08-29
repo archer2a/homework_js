76d646b9c305da526d8ce14d3634fe8e559328fb
archer2a <a.a.apanasenko@gmail.com>
2025-08-29 14:31:16 +0300

    task6

--- lesson-02/03.js
+++ lesson-02/03.js
@@ -4,6 +4,24 @@
  * Результат сохраните в переменной grade.
  */
 
-const score = 70 // тестовое значение, можно изменять
-let grade
+const score = 99 // тестовое значение, можно изменять
+let grade = 0
+
+if (score <= 49) {
+    grade = 'F'
+}
+else if (score <= 69) {
+    grade = 'D'
+}
+else if (score <= 79) {
+    grade = 'C'
+}
+else if (score <= 89) {
+    grade = 'B'
+}
+else if (score <= 100) {
+    grade = 'A'
+}
+
+console.log(grade)
 // your code
