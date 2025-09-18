765d35e3a95c3fbeca72b840d0f3b6dbe07f5e70
archer2a <a.a.apanasenko@gmail.com>
2025-09-18 13:04:19 +0300

    task10

--- lesson-04/01.js
+++ lesson-04/01.js
@@ -13,4 +13,19 @@
 includesElement([1, 2, 3], 2)  // должен вернуть `true`.
 */
 
-function includesElement() {}
+const array = [1, 2, 3, 4, 5];
+
+let element = 0;
+
+function includesElement(arr, element) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === element) {
+            return true;
+        }
+    }
+    return false;
+}
+
+includesElement(array, 5);
+console.log(includesElement(array, 4));
+
